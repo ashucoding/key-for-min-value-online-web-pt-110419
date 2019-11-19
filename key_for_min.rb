9c3ee56efcbc5ac93bb093e7b45db8609354d99b
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-19 01:14:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,17 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
 
 def key_for_min_value(name_hash)
+  
+  if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(ikea)
+
 
 end
